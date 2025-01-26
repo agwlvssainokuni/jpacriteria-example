9640c803e3b3ef00099c11051bd95cdf99e85d52
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2025-01-26 09:12:23 +0900

    EntityManagerを取得方法を見直し。

--- src/main/java/com/example/JpaConfiguration.java
+++ src/main/java/com/example/JpaConfiguration.java
@@ -1,10 +1,18 @@
 package com.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.SharedEntityManagerCreator;
 
 @Configuration
 @EntityScan(basePackages = "com.example.db.entity")
 public class JpaConfiguration {
 
+    @Bean
+    public EntityManager sharedEntityManager(EntityManagerFactory emf) {
+        return SharedEntityManagerCreator.createSharedEntityManager(emf);
+    }
 }
--- src/main/java/com/example/Runner.java
+++ src/main/java/com/example/Runner.java
@@ -1,14 +1,13 @@
 package com.example;
 
 import com.example.db.entity.*;
-import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.JoinType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.ExitCodeGenerator;
-import org.springframework.orm.jpa.EntityManagerFactoryUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,78 +22,75 @@ public class Runner implements ApplicationRunner, ExitCodeGenerator {
     private final CountDownLatch latch = new CountDownLatch(1);
     private final AtomicInteger exitCode = new AtomicInteger(0);
 
-    private final EntityManagerFactory entityManagerFactory;
+    private final EntityManager entityManager;
 
     public Runner(
-            EntityManagerFactory entityManagerFactory
+            EntityManager entityManager
     ) {
-        this.entityManagerFactory = entityManagerFactory;
+        this.entityManager = entityManager;
     }
 
     @Transactional
     @Override
     public void run(ApplicationArguments args) {
-        try (var em = EntityManagerFactoryUtils.getTransactionalEntityManager(entityManagerFactory)) {
 
-            var c = new Customer();
-            c.setFirstName("John");
-            c.setLastName("Doe");
-            em.persist(c);
+        var c = new Customer();
+        c.setFirstName("FirstName");
+        c.setLastName("LastName");
+        entityManager.persist(c);
 
-            var s1 = new SalesOrder();
-            s1.setCustomer(c);
-            s1.setStatus(SalesOrderStatus.NEW);
-            em.persist(s1);
+        var s1 = new SalesOrder();
+        s1.setCustomer(c);
+        s1.setStatus(SalesOrderStatus.NEW);
+        entityManager.persist(s1);
 
-            var s2 = new SalesOrder();
-            s2.setCustomer(c);
-            s2.setStatus(SalesOrderStatus.APPROVED);
-            em.persist(s2);
+        var s2 = new SalesOrder();
+        s2.setCustomer(c);
+        s2.setStatus(SalesOrderStatus.APPROVED);
+        entityManager.persist(s2);
 
-            em.flush();
-            em.clear();
+        entityManager.flush();
+        entityManager.clear();
 
-            var builder = em.getCriteriaBuilder();
-            var criteria1 = builder.createQuery(Customer.class);
+        var builder = entityManager.getCriteriaBuilder();
+        var criteria1 = builder.createQuery(Customer.class);
 
-            var root1 = criteria1.from(Customer.class);
-            root1.fetch(Customer_.salesOrder, JoinType.LEFT);
-            criteria1.select(root1);
+        var root1 = criteria1.from(Customer.class);
+        root1.fetch(Customer_.salesOrder, JoinType.LEFT);
+        criteria1.select(root1);
 
-            try (var result1 = em.createQuery(criteria1).getResultStream()) {
-                result1.forEach(cust -> {
-                    logger.info("Customer: %d %s %s".formatted(cust.getId(), cust.getFirstName(), cust.getLastName()));
-                    cust.getSalesOrder().forEach(order -> {
-                        logger.info("    SalesOrder: %d %s".formatted(order.getId(), order.getStatus()));
-                    });
+        try (var result1 = entityManager.createQuery(criteria1).getResultStream()) {
+            result1.forEach(cust -> {
+                logger.info("Customer: %d %s %s".formatted(cust.getId(), cust.getFirstName(), cust.getLastName()));
+                cust.getSalesOrder().forEach(order -> {
+                    logger.info("    SalesOrder: %d %s".formatted(order.getId(), order.getStatus()));
                 });
-            }
-
-            var criteria2 = builder.createQuery(SalesOrder.class);
-            var root2 = criteria2.from(SalesOrder.class);
-            root2.fetch(SalesOrder_.customer);
-
-            criteria2.select(root2);
-            criteria2.where(
-                    builder.equal(
-                            root2.get(SalesOrder_.status),
-                            SalesOrderStatus.APPROVED
-                    ).not()
-            );
-
-            try (var result2 = em.createQuery(criteria2).getResultStream()) {
-                result2.forEach(so -> {
-                    logger.info("SalesOrder: %d %s, Customer: %d %s %s".formatted(
-                            so.getId(), so.getStatus(),
-                            so.getCustomer().getId(), so.getCustomer().getFirstName(), so.getCustomer().getLastName()
-                    ));
-                });
-            }
-
-            setExitCode(0);
+            });
         }
-    }
 
+        var criteria2 = builder.createQuery(SalesOrder.class);
+        var root2 = criteria2.from(SalesOrder.class);
+        root2.fetch(SalesOrder_.customer);
+
+        criteria2.select(root2);
+        criteria2.where(
+                builder.equal(
+                        root2.get(SalesOrder_.status),
+                        SalesOrderStatus.APPROVED
+                ).not()
+        );
+
+        try (var result2 = entityManager.createQuery(criteria2).getResultStream()) {
+            result2.forEach(so -> {
+                logger.info("SalesOrder: %d %s, Customer: %d %s %s".formatted(
+                        so.getId(), so.getStatus(),
+                        so.getCustomer().getId(), so.getCustomer().getFirstName(), so.getCustomer().getLastName()
+                ));
+            });
+        }
+
+        setExitCode(0);
+    }
 
     private void setExitCode(int code) {
         exitCode.set(code);
